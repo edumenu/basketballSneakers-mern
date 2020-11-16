aff91daa9442a3742e653f7966538192b9501251
Edem Dumenu <ehdemdume@gmail.com>
2020-11-16 17:45:46 -0500

    Footer layout
    Finished the layout for the footer

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -7,13 +7,17 @@ import MainWallpaper from '../../components/MainWallpaper/MainWallpaper';
 import SampleShoes from '../../components/SampleShoes/SampleShoes';
 import VideoHighlights from '../../components/VideoHighlights/VideoHighlights';
 import PlayerStatus from '../../components/PlayerStats/PlayerStatus';
+import Footer from '../../components/Footer/Footer';
 
 const useStyles = makeStyles((theme) => ({
 	root: {
 		flexGrow: 1
 	},
-	paper: {
+	section: {
 		height: '95vh'
+	},
+	footer: {
+		height: '45vh'
 	}
 }));
 
@@ -25,28 +29,28 @@ function Home() {
 			<div>
 				<Grid container direction='column' justify='flex-start' alignItems='stretch'>
 					<Grid item>
-						<Paper className={classes.paper}>
+						<Paper className={classes.section}>
 							<MainWallpaper />
 						</Paper>
 					</Grid>
 					<Grid item>
-						<Paper className={classes.paper} style={{ backgroundColor: '#1f242a' }}>
+						<Paper className={classes.section} style={{ backgroundColor: '#1f242a' }}>
 							<SampleShoes />
 						</Paper>
 					</Grid>
 					<Grid item>
-						<Paper className={classes.paper} style={{ backgroundColor: '#000000' }}>
+						<Paper className={classes.section} style={{ backgroundColor: '#000000' }}>
 							<VideoHighlights />
 						</Paper>
 					</Grid>
 					<Grid item>
-						<Paper className={classes.paper} style={{ backgroundColor: '#1f242a' }}>
+						<Paper className={classes.section} style={{ backgroundColor: '#1f242a' }}>
 							<PlayerStatus />
 						</Paper>
 					</Grid>
 					<Grid item>
-						<Paper className={classes.paper} style={{ backgroundColor: '#3e242c' }}>
-							{/* <PlayerStatus /> */}
+						<Paper className={classes.footer} style={{ backgroundColor: '#3e242c' }}>
+							<Footer />
 						</Paper>
 					</Grid>
 				</Grid>
